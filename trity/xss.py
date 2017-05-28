1dd02cb0d7c35afb4cef0b759d44a827b0b9e880
toxic <filip.herle@gmail.com>
2017-05-27 17:34:16 -0700

    Update xss.py

--- trity/xss.py
+++ trity/xss.py
@@ -1,4 +1,18 @@
 import requests
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   HEADER = '\033[95m'
+   OKBLUE = '\033[94m'
+   OKGREEN = '\033[92m'
 W  = '\033[0m'  # white (normal)
 R  = '\033[31m' # red
 G  = '\033[32m' # green
@@ -14,7 +28,7 @@ def xss():
         content = f.readlines()
     payloads = [x.strip() for x in content]
     print T + "Works best if there is a query at the end. eg. http://example.com?search=" + W
-    url = raw_input("Full URL> ")
+    url = (''+T+'' + color.UNDERLINE + 'Full URL> ' + color.END)
     vuln = []
     for payload in payloads:
         payload = payload
