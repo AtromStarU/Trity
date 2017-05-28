33db160d82bd7a77ecd940ccdeadd47f31a9811b
toxic <filip.herle@gmail.com>
2017-05-27 17:35:10 -0700

    Update sql.py

--- trity/sql.py
+++ trity/sql.py
@@ -1,6 +1,17 @@
 import sys
 import urllib
-
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   HEADER = '\033[95m'
 W  = '\033[0m'  # white (normal)
 R  = '\033[31m' # red
 G  = '\033[32m' # green
